d2b79f94b5fa440f7197e77e0ed5b3c96d4b84f8
sachinpoudel <poudelsachin009@gmail.com>
2025-09-05 21:54:28 +0545

    static prog added

---
+++ staticQn.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+class Counter {
+    static int count;
+
+    public:
+    Counter(){
+        count++;
+    };
+   static void displayCount(){
+        cout<<"The count is "<<count<<endl;
+    };
+};
+int Counter::count=0;
+
+int main(){
+    Counter c1;
+    Counter  c2 ,c3;
+    Counter::displayCount();
+    return 0;
+};

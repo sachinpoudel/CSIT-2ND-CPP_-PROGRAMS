ca7be06ec72655d559224e4760eacbd4a505abe9
sachinpoudel <poudelsachin009@gmail.com>
2025-09-06 21:59:53 +0545

    cmt added

--- 2078.cpp
+++ 2078.cpp
@@ -3,7 +3,7 @@
 //– Create a class Account with data members acc no, balance, and min_balance(static)
 //-Include methods for reading and displaying values of objects
 //– Define static member function to display min_balance
-//-Create array of objects to store data of 5 accounts and read and display values of each object
+//-Create array of objects to store data of 5 accounts and read and display values of each object 
 
 #include<iostream>
 
